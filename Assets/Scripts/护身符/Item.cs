de441621b737dd1245113422819b5e53dfdcdb4f
InYow <2244874962@qq.com>
2024-06-13 19:20:57 +0800

    完成了神符的选择

--- Assets/Scripts/护身符/Item.cs
+++ Assets/Scripts/护身符/Item.cs
@@ -1,14 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Item : MonoBehaviour
+public class Item : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public Sprite sprite;
+    public string desStr;
     public int itemID;
     [Header("GO上的组件")]
     public Image image;
+    [Header("和点击相关")]
+    public bool ed = false;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!ed)
+        {
+            transform.parent = ItemManager.Instance.transform;
+            ItemManager.Instance.itemList.Add(this);
+            ItemChoose._Instance.Close();
+            ed = true;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ItemInfoer.infoer.gameObject.SetActive(false);
+    }
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        //信息面板
+        ItemInfoer.infoer.SetText(this);
+        ItemInfoer.infoer.transform.position = transform.position;
+        ItemInfoer.infoer.gameObject.SetActive(true);
+    }
+
     void Start()
     {
         image = GetComponent<Image>();
